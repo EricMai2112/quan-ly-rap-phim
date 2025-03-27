38bcc1fba6adea2c466603b8e8cd4786493a5127
Binard2112 <mait58674@gmail.com>
2025-03-27 14:59:21 +0700

    add libraries and ConnectDB

---
+++ src/main/java/ConnectDB/ConnectDB.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ConnectDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Admin
+ */
+public class ConnectDB {
+	public static Connection getConnection() {
+        Connection connection = null;
+        try {
+        	String url = "jdbc:sqlserver://localhost:1433;databaseName=QuanLyRapChieuPhim;encrypt=false";
+            String username = "sa";
+            String password = "123";
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+	public static void closeConnection(Connection conn, Statement stmt, ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
