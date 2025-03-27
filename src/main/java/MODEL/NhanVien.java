9f150c60a3534dda13cb039a56bfef9a5013790e
Binard2112 <mait58674@gmail.com>
2025-03-27 14:08:48 +0700

    change attributes NhanVien

--- src/main/java/MODEL/NhanVien.java
+++ src/main/java/MODEL/NhanVien.java
@@ -1,55 +1,98 @@
 package MODEL;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class NhanVien {
 	private String maNHanVien;
 	private String hoTen;
-	private String tenDangNhap;
-	private String matKhau;
+	private Date ngaySinh;
+	private String soDienThoai;
+	private String cccd;
 	private VaiTro vaiTro;
-	public NhanVien(String maNHanVien, String hoTen, String tenDangNhap, String matKhau, VaiTro vaiTro) {
+	
+	public NhanVien(String maNHanVien, String hoTen, Date ngaySinh, String soDienThoai, String cccd, VaiTro vaiTro) {
 		super();
 		this.maNHanVien = maNHanVien;
 		this.hoTen = hoTen;
-		this.tenDangNhap = tenDangNhap;
-		this.matKhau = matKhau;
+		this.ngaySinh = ngaySinh;
+		this.soDienThoai = soDienThoai;
+		this.cccd = cccd;
 		this.vaiTro = vaiTro;
 	}
+	
 	public String getMaNHanVien() {
 		return maNHanVien;
 	}
+
 	public void setMaNHanVien(String maNHanVien) {
 		this.maNHanVien = maNHanVien;
 	}
+
 	public String getHoTen() {
 		return hoTen;
 	}
+
 	public void setHoTen(String hoTen) {
 		this.hoTen = hoTen;
 	}
-	public String getTenDangNhap() {
-		return tenDangNhap;
+
+	public Date getNgaySinh() {
+		return ngaySinh;
 	}
-	public void setTenDangNhap(String tenDangNhap) {
-		this.tenDangNhap = tenDangNhap;
+
+	public void setNgaySinh(Date ngaySinh) {
+		this.ngaySinh = ngaySinh;
 	}
-	public String getMatKhau() {
-		return matKhau;
+
+	public String getSoDienThoai() {
+		return soDienThoai;
 	}
-	public void setMatKhau(String matKhau) {
-		this.matKhau = matKhau;
+
+	public void setSoDienThoai(String soDienThoai) {
+		this.soDienThoai = soDienThoai;
 	}
+
+	public String getCccd() {
+		return cccd;
+	}
+
+	public void setCccd(String cccd) {
+		this.cccd = cccd;
+	}
+
 	public VaiTro getVaiTro() {
 		return vaiTro;
 	}
+
 	public void setVaiTro(VaiTro vaiTro) {
 		this.vaiTro = vaiTro;
-	}
-	@Override
-	public String toString() {
-		return "NhanVien [maNHanVien=" + maNHanVien + ", hoTen=" + hoTen + ", tenDangNhap=" + tenDangNhap + ", matKhau="
-				+ matKhau + "]";
 	}
 	
 	
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(maNHanVien);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NhanVien other = (NhanVien) obj;
+		return Objects.equals(maNHanVien, other.maNHanVien);
+	}
+
+	@Override
+	public String toString() {
+		return "NhanVien [maNHanVien=" + maNHanVien + ", hoTen=" + hoTen + ", ngaySinh=" + ngaySinh + ", soDienThoai="
+				+ soDienThoai + ", cccd=" + cccd + ", vaiTro=" + vaiTro + "]";
+	}
+
+	
 }
